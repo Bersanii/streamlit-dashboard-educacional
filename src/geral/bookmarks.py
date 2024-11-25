6c91a25971ffea41675a866ca9a0336808813f6b
Vitor Bersani <vitor.bersani@mutant.com.br>
2024-11-25 09:09:47 -0300

    atualização em bookmarks

--- src/geral/bookmarks.py
+++ src/geral/bookmarks.py
@@ -9,3 +9,17 @@ query = f'''
 
 df = database.connection.run_query(query, True)
 st.dataframe(df, use_container_width=True)
+
+# Seleção do bookmark para remoção
+bookmark_id = st.selectbox('Escolha um bookmark para remover', df['id'].tolist())
+
+# Botão de remoção
+if st.button('Remover Bookmark'):
+  if bookmark_id:
+    # Executando a query de remoção
+    delete_query = f'DELETE FROM bookmark WHERE id = {bookmark_id} AND id_usuario = {st.session_state.usuario['ID']}'
+    try:
+      database.connection.run_query(delete_query, False)
+      st.success('Bookmark removido com sucesso!')
+    except Exception as e:
+      st.error(f'Erro ao remover o bookmark: {e}')
